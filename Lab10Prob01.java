fab5e2c3397ae4f2d73c0d71faddb9c40bd44ff1
tp13828 <150047822+tp13828@users.noreply.github.com>
2023-11-17 13:53:31 -0500

    Add files via upload

---
+++ Lab10Prob01.java
@@ -0,0 +1,12 @@
+/**
+* File: Lab 10 Prob01.java
+* Class: CSCI 1302
+* Author: Stella Pham and Thi Tran
+* Created on: Nov 17, 2023
+* Last Modified: Nov 17, 2023
+* Description: File reading people.dat
+*/
+
+public class Lab10Prob01 {
+
+}
